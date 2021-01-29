9555c5200832575f895d6f7f02b721545a391467
RITA UY <rita.uy56@gmail.com>
2021-01-29 15:25:25 +1100

    removed comments

--- ThisIsAnElevator/Lift.py
+++ ThisIsAnElevator/Lift.py
@@ -11,13 +11,8 @@ class LiftClass:
         self.queue = LiftClass.initQue()
 
     def createFloors(levels):
-        # How many floors are there?
-        # This should give me a list of floors. 
-        #I assume that all the floors are sequential
-        
         floors = []
         i = 0
-        #I assume 0 is level "G"
         while i <= levels:
             floors.append(i)
             i += 1
@@ -44,17 +39,10 @@ class LiftClass:
         return self.queue
 
     def stopLift():
-        # There should be a timer here in this project, something that moves the lift
-        # up and down
-        # This should open doors and have a wait
-        # Should it return something?
         print("lift stopped")
 
     
     def moveUp(self):
-        # This will travel to every floor.
-        # Program is not efficient
-        # There needs to be a range here
         top = self.queue[len(self.queue) - 1]
         bottom = self.queue[0]
 
