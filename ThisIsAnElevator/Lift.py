80839187e914a7c4dcb85f4a5ca5595e5c7b4ef8
RITA UY <rita.uy56@gmail.com>
2021-01-29 11:18:25 +1100

    I have implemented a timer, and it works. The lift moves up, but thats about it at the moment

--- ThisIsAnElevator/Lift.py
+++ ThisIsAnElevator/Lift.py
@@ -1,4 +1,6 @@
 import enum
+import time 
+
 class ModeClass(enum.Enum):
     UP = 0
     DOWN = 1
@@ -29,24 +31,39 @@ class LiftClass:
     def requestFloor(floor, queue):
         queue.append(floor)
         queue.sort()
+        print("floor", floor, "requested")
         return queue
-
-    def moveUp(floors, queue):
-        for i in floors:
-            if i in queue:
-                stopLift()
-
-
     
-    def moveDown():
-        pass
-
     def stopLift():
         # There should be a timer here in this project, something that moves the lift
         # up and down
         # This should open doors and have a wait
         # Should it return something?
+        print("lift stopped")
+
+    
+    def moveUp(floors, queue):
+        for i in floors:
+            print("lift is at:", i)
+            if i in queue:
+                print("Lift is stopping")
+                timeLift = 3
+                while timeLift >= 0:
+                    LiftClass.stopLift()
+                    #Let the lift wait for 3 seconds
+                    if LiftClass.liftTimer(timeLift) >= 0:
+                        timeLift -= 1
+
+    def liftTimer(timeLift):
+        t = timeLift
+        myText = "Time: 00:{:02d}"
+        print(myText.format(t), end='\r')
+        time.sleep(1)
+        return t
+
+    def moveDown():
         pass
+
 
     def moveManager():
         pass
--- ThisIsAnElevator/TestCase.py
+++ ThisIsAnElevator/TestCase.py
@@ -3,15 +3,20 @@ from LiftManager import *
 
 # Tests
   
-try:
-    levels = LiftClass.createFloors(6)
-    #up = LiftClass.initUp()
-    #down = LiftClass.initDown()
-    LiftClass.showListOfFloors(levels) 
-except:
-    print("Stage 1 program has failed")
-else:
-    print("Stage 1 is successful")
+levels = LiftClass.createFloors(6)
+sequence = LiftClass.initQue()
+#up = LiftClass.initUp()
+#down = LiftClass.initDown()
+LiftClass.showListOfFloors(levels) 
+LiftClass.requestFloor(2, sequence)
+LiftClass.moveUp(levels, sequence)
+#try:
+    
+
+#except:
+#    print("Stage 1 program has failed")
+#else:
+#    print("Stage 1 is successful")
 
 
 
